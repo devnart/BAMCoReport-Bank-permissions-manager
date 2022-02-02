1043d96ad21726c123ce4b326bc5f9b106052764
salmakl <salma.kalkhi@gmail.com>
2022-02-02 20:18:53 +0100

    crud user membership

--- src/main/java/com/bam/bamcoreport/service/UserMembershipService.java
+++ src/main/java/com/bam/bamcoreport/service/UserMembershipService.java
@@ -1,5 +1,33 @@
 package com.bam.bamcoreport.service;
 
 
+import com.bam.bamcoreport.entity.UserMembership;
+import com.bam.bamcoreport.repository.UserMembershipRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserMembershipService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserMembershipService.class);
+
+    @Autowired
+    UserMembershipRepository userMembershipRepository;
+
+    public UserMembershipService(UserMembershipRepository userMembershipRepository) {
+        this.userMembershipRepository = userMembershipRepository;
+    }
+
+    public List<UserMembership> getAll(){
+        return userMembershipRepository.findAll();
+    }
+
+    public void add(UserMembership userMembership){
+        userMembershipRepository.save(userMembership);
+    }
 }
+
