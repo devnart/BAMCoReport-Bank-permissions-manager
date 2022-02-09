268e67d6094aa4d32972a5cd45f0b5cf18252a3e
salmakl <salma.kalkhi@gmail.com>
2022-02-09 09:33:22 +0100

    Reject

---
+++ src/main/java/com/bam/bamcoreport/controller/RejectController.java
@@ -0,0 +1,9 @@
+package com.bam.bamcoreport.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/Reject")
+public class RejectController {
+}
