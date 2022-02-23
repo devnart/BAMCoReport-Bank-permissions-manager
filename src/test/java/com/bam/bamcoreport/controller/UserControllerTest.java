cd4c6c89e314c2f3ba8a0d0c53c0b6685ac55d25
devnart <hr.fb001@gmail.com>
2022-02-23 10:44:17 +0100

    testing

--- src/test/java/com/bam/bamcoreport/controller/UserControllerTest.java
+++ src/test/java/com/bam/bamcoreport/controller/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.bam.bamcoreport.dto.model.UserDto;
 import com.bam.bamcoreport.entity.Users;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserControllerTest {
 
     @Autowired
+    @Mock
     private UserController userController;
 
     @Test
